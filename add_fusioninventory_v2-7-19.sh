ace1a21a11c3d919250d1d3e85e26395de9be4bc
im just a flower <53564088+periwinkleispurple@users.noreply.github.com>
2019-11-05 13:11:29 +0100

    Update add_fusioninventory_v2-7-19.sh

--- add_fusioninventory_v2-7-19.sh
+++ add_fusioninventory_v2-7-19.sh
@@ -5,23 +5,24 @@ echo
 echo 
 echo Installing FusionInventory plugin version glpi9.4.0+1.0.
 echo
-echo Recommended OS : debian-10.0.0-amd64.
+echo Working OS : debian-10.0.0-amd64 or Mint 'Sarah'.
+echo Recommended GLPI version : v 9.4.3 
 echo
 echo
-echo
+echo Starting...
 sleep 5s
 
 DIR="/var/www/html/glpi/plugins/"
 
 if [ -d "$DIR" ]; then
   	cd /tmp
-	wget sewesty.com/fusioninventory/glpi9.4.0+1.0.tar.gz
+	wget periwinkle.ovh/library/glpi/glpi9.4.0+1.0.tar.gz
 	tar xvf glpi9.4.0+1.0.tar.gz -C /var/www/html/glpi/plugins
 	chown -R www-data /var/www/html/glpi/plugins
 	cd /var/www/html/glpi/plugins
   	echo "Installing fusioninventory plugin in ${DIR}..."
 else
-  	echo "Error: ${DIR} not found. GLPI is not installed, use my glpi_standalone_v-1-7-19.sh or glpi_fusioninventory_v1-7-19.sh"
+  	echo "Error: ${DIR} not found. GLPI is not installed, use my glpi tutorial or script"
   	exit 1
 fi
 
