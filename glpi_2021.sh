9f4541598b05831def30af0282acd6b7567725fb
im just a flower <53564088+redframboise@users.noreply.github.com>
2021-07-28 15:06:36 +0200

    Update glpi_2021.sh

--- glpi_2021.sh
+++ glpi_2021.sh
@@ -14,3 +14,20 @@ systemctl enable mariadb.service
 echo "CREATE DATABASE glpi" | mysql -u root 
 echo "CREATE USER 'glpibdd'@'localhost' IDENTIFIED BY 'glpibddpass';" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON glpi. * TO 'glpibdd'@'localhost';" | mysql -u root
+
+wget https://github.com/glpi-project/glpi/releases/download/9.5.5/glpi-9.5.5.tgz
+tar xzf glpi-9.5.5.tgz
+cp -R glpi /usr/share
+chown -R root.www-data /usr/share/glpi
+chown -R www-data /var/www/glpi
+chmod -R 775 /usr/share/glpi
+ln -s /usr/share/glpi /var/www/html/
+/etc/init.d/apache2 restart
+
+hostname -i 
+x.x.x.x/glpi
+usr : glpi
+pwd : glpi
+db : localhost
+dbuser : glpibdd
+dbpwd : glpibddpwd
