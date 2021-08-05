7624bf1f2e5c52a2cd1ff677a9954364f4ab3866
im just a flower <53564088+redframboise@users.noreply.github.com>
2021-08-05 15:19:23 +0200

    Update set-ip.sh

--- set-ip.sh
+++ set-ip.sh
@@ -7,5 +7,7 @@ iface enp0s3 inet static
     address 192.168.1.34/24
     gateway 192.168.1.254
     
-sudo ifdown enp0s3
+# Pour save et quitter nano Ctrl+X -> O -> Entrer
+
+sudo ifdown enp0s3 # Si erreur -> changer d'adresse, nano /etc/network/interfaces -> sudo systemctl restart networking
 sudo systemctl restart networking
