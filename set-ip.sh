a6fb5084846b52a582019e3cc4f57efbb95683a4
im just a flower <53564088+redframboise@users.noreply.github.com>
2021-08-05 15:13:34 +0200

    Update set-ip.sh

--- set-ip.sh
+++ set-ip.sh
@@ -1,1 +1,11 @@
-x
+cp /etc/network/interfaces /etc/network/interfaces.origin
+nano /etc/network/interfaces
+
+# Interface enp0s3
+auto enp0s3
+iface enp0s3 inet static
+    address 192.168.1.34/24
+    gateway 192.168.1.254
+    
+ifdown enp0s3
+systemctl restart networking
